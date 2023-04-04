a74bcfb4ed49cc3509bd8bf28fe2a1b0a5290eaa
Shahrez7 <116435332+Shahrez7@users.noreply.github.com>
2023-04-04 14:20:52 +0500

    File Made

---
+++ Assign1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <conio.h>
+
+using namespace std;
+
+const int ROW = 4;
+const int COL = 4;
+int wall[ROW][COL];
+int score = 0;
+int highscore=0;
