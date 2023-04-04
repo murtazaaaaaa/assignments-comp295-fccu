dbd84e82bf3ade40c9fccab1e35cdc91bd17cf73
Shahrez7 <116435332+Shahrez7@users.noreply.github.com>
2023-04-04 14:21:47 +0500

    Updated

--- Assign1.cpp
+++ Assign1.cpp
@@ -10,3 +10,25 @@ const int COL = 4;
 int wall[ROW][COL];
 int score = 0;
 int highscore=0;
+void gameWall() {
+    srand(time(NULL));
+    int row1 = rand() % ROW;
+    int col1 = rand() % COL;
+    int row2 = rand() % ROW;
+    int col2 = rand() % COL;
+    int num1 = rand() % 2;
+    int num2 = rand() % 2;
+    if (num1 == 0){
+        wall[row1][col1] = 2;
+    }
+    else{
+        wall[row1][col1] = 4;
+    }
+    if (num2 == 0){
+        wall[row2][col2] = 4;
+    }
+    else{
+        wall[row2][col2] = 2;
+    }
+}
+
